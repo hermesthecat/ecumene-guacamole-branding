eed862b1f14853d6d1a0c9fff7d5bf42ae8f839e
Michael White <mwhite@signal-fire.com>
2022-11-16 17:03:49 -0500

    Initial finished.  Pre cleanup and refactor

--- branding/js/main.js
+++ branding/js/main.js
@@ -1,34 +1,3 @@
-const wallpapers = {
-    blue1: {
-        url: 'app/ext/ecumene/images/blue2.jpg',
-        theme: 'light',
-        windowColor: 'rgba(255,255,255,0.05)',
-        windowBrightness: '1.3',
-        //windowBrightness: '1.3',
-    },
-    reach: {
-        url: 'app/ext/ecumene/images/reach.jpg',
-        theme: 'light',
-        windowColor: 'rgba(255,255,255,0.15)',
-        windowBrightness: '1.1',
-        //windowBrightness: '1.3',
-    },
-}
 
-const randomProperty = (obj) => {
-    let keys = Object.keys(obj);
-    return obj[keys[ keys.length * Math.random() << 0]];
-};
 
-const bg = randomProperty(wallpapers)
 
-//let elem = document.querySelector('div.login-ui');
-//elem.style.backgroundImage= bg.url;
-$(document).ready(() => {
-    $('.app-form-field input').click((e) => {
-        $(e.target.parentElement.parentElement.parentElement).find('label').addClass('active');
-    })
-    $('.app-form-field input').focusout((e) => {
-        $(e.target.parentElement.parentElement.parentElement).find('label').removeClass('active');
-    })
-});
